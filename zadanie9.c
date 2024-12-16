5543d8e43c070878dd646888a422bb1fa137d5ec
sandra698 <sandrajump51@gmail.com>
2024-12-17 00:07:34 +0300

    Добавлен отчет 9

--- zadanie9.c
+++ zadanie9.c
@@ -4,14 +4,14 @@
 #include <math.h>
 #include <time.h>
 
-#define MATRIX_SIZE 600      // Размер матрицы
-#define BLOCK_SIZE 10        // Размер блока
+#define MATRIX_SIZE 600      
+#define BLOCK_SIZE 10        
 
 int main(int argc, char **argv)
     {
-    int rank, size;               // Ранг процесса и общее количество процессов
+    int rank, size;               
     int rank_in_row, rank_in_col; // Ранги в строковых и столбцовых группах
-    int grid_size;                // Размер решётки процессов
+    int grid_size;                
     MPI_Comm row_communicator, col_communicator; // Коммуникаторы для строк и столбцов
 
     MPI_Init(&argc, &argv);
@@ -33,14 +33,15 @@ int main(int argc, char **argv)
     // Разделяем процессы на строковые и столбцовые группы
     int row_id = rank / grid_size;
     int col_id = rank % grid_size;
+    //создание продгруп с помощью split
     MPI_Comm_split(MPI_COMM_WORLD, row_id, rank, &row_communicator);
     MPI_Comm_split(MPI_COMM_WORLD, col_id, rank, &col_communicator);
-
+    //получили ранги новых в новых  коммуникаторах
     MPI_Comm_rank(row_communicator, &rank_in_row);
     MPI_Comm_rank(col_communicator, &rank_in_col);
 
     // Размер части матрицы для каждого процесса
-    int size_part_matrix = MATRIX_SIZE * MATRIX_SIZE / size;
+    int size_part_matrix = MATRIX_SIZE * MATRIX_SIZE / size;//size_part_matrix - количество элементов
 
     // Выделение памяти для матриц
     int *matrix_A = (int*)malloc(size_part_matrix * sizeof(int));
@@ -51,6 +52,7 @@ int main(int argc, char **argv)
     int *current_block_row = (int*)malloc(MATRIX_SIZE * BLOCK_SIZE / grid_size * sizeof(int));
     int *current_block_col = (int*)malloc(MATRIX_SIZE * BLOCK_SIZE / grid_size * sizeof(int));
 
+//current_block_row и current_block_col- массивы использ для хранения блоков строк их матрицы B и блоков столбцов из матрицы A которые будут передаваться между собой
     // Инициализация матриц A и B случайными значениями
     srand(time(NULL) + rank);
     for (int i = 0; i < size_part_matrix; i++)
@@ -72,7 +74,10 @@ int main(int argc, char **argv)
                 {
                 current_block_row[l] = matrix_B[(block_index % (MATRIX_SIZE / grid_size / BLOCK_SIZE)) * (MATRIX_SIZE / grid_size) + l];
                 }
-        }
+        } 
+	//rank_in_row- проверяет, что текущий процесс ответственный за подготовку строки для передачи
+	//current_block_row- копирует блок строки из матрицы В
+	//MPI_Bcast- передает блоки строки current_block_row  всем проуессам в той же строке
         MPI_Bcast(current_block_row, MATRIX_SIZE * BLOCK_SIZE / grid_size, MPI_INT, block_index / (MATRIX_SIZE / grid_size), row_communicator);
 
 	// Подготовка столбца матрицы A для передачи
@@ -84,15 +89,17 @@ int main(int argc, char **argv)
                                                 (l / BLOCK_SIZE) * (MATRIX_SIZE / grid_size) + l % BLOCK_SIZE];
                 }
             }
-        MPI_Bcast(current_block_col, MATRIX_SIZE * BLOCK_SIZE / grid_size, MPI_INT, block_index / (MATRIX_SIZE / grid_size), col_communicator);
 
+	//ank_in_col- проверяет что текущий процесс ответственный за подготовку столбца
+        MPI_Bcast(current_block_col, MATRIX_SIZE * BLOCK_SIZE / grid_size, MPI_INT, block_index / (MATRIX_SIZE / grid_size), col_communicator);
+//MPI_Bcast- передает блок столбца current_block_col всем процессам в том же столбце 
 	// Вычисление части матрицы C
-        for (int x = 0; x < MATRIX_SIZE / grid_size; x++)
+        for (int x = 0; x < MATRIX_SIZE / grid_size; x++)//двойной цикл перебирает все элементы локального блока матрицы С
            {
             for (int y = 0; y < MATRIX_SIZE / grid_size; y++)
                 {
                 int chast_sum = 0;
-                for (int h = 0; h < BLOCK_SIZE; h++)
+                for (int h = 0; h < BLOCK_SIZE; h++)//выполняет суммирование произведений элементов блоков строки и столбца
                     {
                     chast_sum += current_block_col[y * BLOCK_SIZE + h] * current_block_row[h * (MATRIX_SIZE / grid_size) + x];
                     }
@@ -100,7 +107,7 @@ int main(int argc, char **argv)
                 }
             }
     }
-
+//matrix_C- к результату добавляется вычисленная сумма
     if (rank == 0)
         {
         printf("Execution time: %lf seconds\n", MPI_Wtime() - start_time);
